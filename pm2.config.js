cee6ea503b75292a052e0322e1fceea14b55140d
the main thing <pelagusrem@gmail.com>
2024-11-20 19:26:33 +0300

    removed custom hosting things. I remembered I can host for free on fly.io. Also I've commited database :facepalm:

--- pm2.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'lazy-tarot-api', // Name of your application
-	script: 'src/index.ts', // Entry point of your application
-	interpreter: 'bun', // Bun interpreter
-	env: {
-		PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
-	},
-}
