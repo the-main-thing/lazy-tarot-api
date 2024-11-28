5957c4c5d1ab00c7bb64ecfebecda6578583de97
the main thing <pelagusrem@gmail.com>
2024-11-28 13:52:33 +0300

    custom vps setup

--- src/routes/updater.ts
+++ src/routes/updater.ts
@@ -14,6 +14,15 @@ const cmd = (command: string) => {
   })
 }
 
+const update = async () => {
+  await cmd('git pull')
+  await cmd('bun install')
+  await cmd('bun run migrations-push')
+  exec('pm2 restart tarot-api')
+}
+
+update()
+
 export const updater = async (request: Request) => {
   if (request.headers.get('x-api-key') !== env.AUTOMATION_API_KEY) {
     return
