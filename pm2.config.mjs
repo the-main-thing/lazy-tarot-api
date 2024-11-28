4c56049864b8922cc2f87fb0fbe776cd079fd5bd
the main thing <pelagusrem@gmail.com>
2024-11-28 14:17:59 +0300

    custom vps setup

--- pm2.config.mjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "tarot-api", // Name of your application
-  script: "src/server.bun.ts", // Entry point of your application
-  interpreter: "bun", // Bun interpreter
-  env: {
-    PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
-  }
-};
