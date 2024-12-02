6f190b2ce2a603882bbaac9bc77ce18f277029ee
the main thing <pelagusrem@gmail.com>
2024-12-02 17:35:09 +0300

    CORS for capacitor

--- src/server.bun.ts
+++ src/server.bun.ts
@@ -4,15 +4,34 @@ import { router } from './routes/router'
 import { server as serverGlobal } from './server'
 import { translationsWebsocketConfig } from './routes/translations'
 
+const allowedOrigins = ['capacitor://localhost', 'ionic://localhost']
+
+const addCors = (request: Request, response: Response) => {
+  const origin = new URL(
+    request.headers.get('origin') ||
+      'http://this-is-definately-not-allowed.com',
+  ).origin
+  if (allowedOrigins.includes(origin)) {
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    response.headers.set(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization, x-api-key',
+    )
+    response.headers.set('Access-Control-Allow-Origin', origin)
+    response.headers.set('Transfer-Encoding', 'chunked')
+  }
+  return response
+}
+
 const run = () => {
   const server = Bun.serve({
     port: env.PORT || 3000,
-    fetch(request) {
+    async fetch(request) {
       try {
-        return router(request)
+        return addCors(request, await router(request))
       } catch (error) {
         if (error instanceof Response) {
-          return error
+          return addCors(request, error)
         }
         log.error(
           'Error handling request',
@@ -21,7 +40,7 @@ const run = () => {
           '\n',
           error,
         )
-        return new Response('Internal error', { status: 500 })
+        return addCors(request, new Response('Internal error', { status: 500 }))
       }
     },
     websocket: translationsWebsocketConfig,
