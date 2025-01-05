adf2826503bc4ac6909a8147700af7b10ae0f5b4
the main thing <pelagusrem@gmail.com>
2025-01-06 00:01:08 +0400

    how come that I test in prod?

--- src/routes/hasValidKey.ts
+++ src/routes/hasValidKey.ts
@@ -1,7 +1,7 @@
 import { session } from './session'
 import { env } from '../env'
 
-const VALID_KEYS = [env.LAZY_TAROT_API_KEY, env.MOBILE_CLIENT_API_KEY]
+const VALID_KEYS = [env.LAZY_TAROT_API_KEY, env.MOBILE_CLIENT_API_KEY, env.AUTOMATION_API_KEY]
 export const hasValidKey = (request: Request) =>
   VALID_KEYS.includes(request.headers.get('x-api-key') as never) ||
   session.isValid(request.headers.get('cookie')) ||
